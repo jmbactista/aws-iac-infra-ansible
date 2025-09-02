7176025cf238fd7af53f73b554d54d6464f2b265
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 05:37:23 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-#resource "aws_s3_bucket" "terraform_state" {
-#  bucket = "s3s3-bucket-tfstate-test"
-#  acl    = "private"
-
-#  versioning {
-#    enabled = true
-#  }
-
-#  tags = {
-#    Name = "Terraform State"
-#  }
-#}
-
 terraform {
   backend "s3" {
     bucket = "s3-bucket-tfstate-test"
