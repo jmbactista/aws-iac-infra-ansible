cf546717801ba962eeb98aca59618bffd99f3ea7
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 04:08:56 +0800

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,7 +13,8 @@ resource "aws_instance" "bastion" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = aws_key_pair.bastion_key.key_name
-  security_groups = [aws_security_group.bastion_sg.name]
+  #security_groups = [aws_security_group.bastion_sg.name]
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   tags = {
     Name = "Bastion Host"
@@ -25,7 +26,8 @@ resource "aws_instance" "resource_server" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
   key_name      = aws_key_pair.bastion_key.key_name
-  security_groups = [aws_security_group.resource_sg.name]
+  #security_groups = [aws_security_group.resource_sg.name]
+  vpc_security_group_ids = [aws_security_group.resource_sg.id]
 
   tags = {
     Name = "Resource Server"
