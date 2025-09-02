bbb6cf324f4365628d40e3de8ec427bae21b9e47
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 03:44:40 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,24 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3-bucket-tfstate"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "Terraform State"
+  }
+}
+
 terraform {
   backend "s3" {
-    bucket         = "s3-backup"
-    key            = "terraform.tfstate"
-    region         = "ap-southeast-1"
-    encrypt        = true
+    bucket = aws_s3_bucket.terraform_state.bucket
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1" 
+    encrypt = true
   }
 }
