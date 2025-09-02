2cb654ea1976ae3a447274d616317a5bb94754f6
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 00:40:25 +0800

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -22,3 +22,43 @@ resource "aws_subnet" "private_subnet" {
     Name = "Private Subnet"
   }
 }
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH access from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict as per your requirement
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "resource_sg" {
+  name        = "resource-sg"
+  description = "Allow internal communication from bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
