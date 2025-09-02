41b7c17c27cd6b5b491175c9a126c54821a0225e
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 02:10:27 +0800

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,13 @@
+resource "tls_private_key" "bastion" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "bastion_key" {
+  key_name   = "bastion-key"
+  public_key = tls_private_key.bastion.public_key_openssh
+}
+
 resource "aws_instance" "bastion" {
   ami           = "ami-0b3e5e8d5d8bcd15d" 
   instance_type = "t2.micro"
@@ -14,6 +24,7 @@ resource "aws_instance" "resource_server" {
   ami           = "ami-0b3e5e8d5d8bcd15d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
+  key_name      = aws_key_pair.bastion_key.key_name
   security_groups = [aws_security_group.resource_sg.name]
 
   tags = {
