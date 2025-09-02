c642d798c278f89f2766eddb97b16d2d12d646a3
jmbactista <37825770+jmbactista@users.noreply.github.com>
2025-09-03 02:07:06 +0800

    Update kp.tf

--- terraform/kp.tf
+++ terraform/kp.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "bastion_key" {
-  key_name   = "bastion-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
+#resource "aws_key_pair" "bastion_key" {
+#  key_name   = "bastion-key"
+#  public_key = file("~/.ssh/id_rsa.pub")
+#}
